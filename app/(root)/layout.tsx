1141b4b29dd41a36ccd8932c94042d799fdd6d3f
ashiii1 <aashishaik526@gmail.com>
2024-06-12 20:21:53 +0530

    shedule meeting feature

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,15 @@
 import React, { ReactNode } from 'react'
 import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next'
 
+
+export const metadata: Metadata={
+  title:"YOOM",
+
+description:"VIDEO APP",
+icons:{
+  icon: '/icons/logo.svg'
+}}
 const RootLayout = ({children}: {children : ReactNode}) => {
   return (
     <main>
