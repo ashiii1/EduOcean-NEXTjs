e86192b5664408b23be279bda1e6dbba3cbf92e0
ashiii1 <aashishaik526@gmail.com>
2024-06-12 19:50:53 +0530

    FINALLY

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,10 @@ import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/toaster";
+import "./globals.css";
 
+//import '@stream-io/video-react-sdk/dist/css/style.css'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,11 +22,25 @@ export default function RootLayout({
 }: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en">
-      <ClerkProvider>
-       
+      <ClerkProvider
+        appearance={{
+          layout: {
+            socialButtonsVariant: "iconButton",
+            logoImageUrl: "/icons/yoom-logo.svg",
+          },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1C1F2E",
+            colorInputBackground: "#252A41",
+            colorInputText: "#fff",
+          },
+        }}
+      >
         <body className={`${inter.className} bg-dark-2`}>
           
           {children}
+          <Toaster/>
         </body>
       </ClerkProvider>
     </html>
